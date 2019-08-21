5f176d4bf751c23b36e2188bddd132165cfa29e4
atkinst2 <54369172+atkinst2@users.noreply.github.com>
2019-08-21 12:12:07 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello 
+	{
+		public static void printHeading()
+			{
+				System.out.println("TimOthy Atkins");
+				System.out.println("Lab Uno");
+				System.out.println("Project Description: Getting started on something called Java");
+				System.out.println("August 21, 2019 - I think? ");
+				System.out.println("CMSC-255-0304-2019Fall");
+				System.out.println();
+			}
+		public static void main(String [] args)
+			{
+				printHeading();
+				System.out.println("Wassup World");
+			}
+	}
